c89a20878cd7c247b75082d653e1b2c1e1f7451e
Abhilash437 <abhilashhathwar20@gmail.com>
2022-12-10 18:53:43 +0530

    temp commit

---
+++ YouToobe/my-app/App.js
@@ -0,0 +1,45 @@
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+// import * as React from 'react';
+import React from 'react';
+import {WebView} from 'react-native-webview';
+
+export default class App extends React.Component{
+  render(){
+    return (
+      <WebView source={{uri:'https://main.dvr3qmfkxpsdn.amplifyapp.com'}} style={{marginTop:30}}/>
+    )
+  }
+}
+
+
